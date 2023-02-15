38fa2b4f703580e1bc3d141abb1b9c50cee0908a
Igor ZLOBINE <izlobine@gmail.com>
2023-02-15 07:15:11 +0100

    stubs

--- src/Controller/StubController.php
+++ src/Controller/StubController.php
@@ -10,6 +10,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
